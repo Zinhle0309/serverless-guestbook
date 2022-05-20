09e57696f447c57bd7db0801bdc547ddb096265d
Zinhle0309 <72995433+Zinhle0309@users.noreply.github.com>
2022-05-20 09:40:06 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://24d27538.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
